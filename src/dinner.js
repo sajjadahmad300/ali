fb89c5946fef17517a0792bd756ca98ffce22a00
sajjad ahmad <sajjad.ahmad300@gmail.com>
2020-09-18 23:01:58 -0700

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -3,7 +3,7 @@ function Dinner() {
   return (
     <div>
       <h1>Today we are serving </h1>
-      <h1>Today we are also serving sweet dish </h1>
+      <h1>Today we are also serving sweefsddggt dish </h1>
     </div>
   );
 }
