66cd1c60dffd3415ee4264f08168ff86c5e1b866
sajjad ahmad <sajjad.ahmad300@gmail.com>
2020-09-18 19:51:40 -0700

    2nd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Dinner';
 
 function App() {
   return (
     <div className="App">
-      
+      <Dinner dishName='Nihaari' sweetDish='Kheer' />
+      <hr />
+      <Dinner dishName='Biryani' sweetDish='Jaleebi' />
+      <hr />
+      <Dinner dishName='Karahi' sweetDish='Gajar ka Halwa' />
     </div>
   );
 }
