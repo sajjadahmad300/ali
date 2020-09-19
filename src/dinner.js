023261c35337e1a51e83d2e9c85eb37e0efdcea9
sajjad ahmad <sajjad.ahmad300@gmail.com>
2020-09-18 19:29:57 -0700

    1st

---
+++ src/dinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Dinner() {
+  return (
+    <div>
+      <h1>Today we are serving </h1>
+      <h1>Today we are also serving sweet dish </h1>
+    </div>
+  );
+}
+export default Dinner;
